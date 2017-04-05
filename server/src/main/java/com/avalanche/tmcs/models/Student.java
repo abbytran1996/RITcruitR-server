9f557751952db24fca88a80f66e90e858080fc47
David Dubois <dexcelstraun7@gmail.com>
2017-04-05 16:03:08 -0400

    Added fields to Student model

--- server/src/main/java/com/avalanche/tmcs/models/Student.java
+++ server/src/main/java/com/avalanche/tmcs/models/Student.java
@@ -1,8 +1,141 @@
 package com.avalanche.tmcs.models;
 
+import javax.persistence.*;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import java.sql.Date;
+
 /**
+ * Class to represent a student in the database
+ *
  * @author David Dubois
- * @date 05-Apr-17.
+ * @since 05-Apr-17.
  */
+@Entity
+@Table(name="students")
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    private String firstName;
+
+    @NotNull
+    private String lastName;
+
+    // TODO: Uncomment when we write the Skills model
+    // private List<Skill> skills
+
+    @NotNull
+    // Emails have something, then an @ sign, then something, then a period, then something
+    // This is possibly overly broad, but students have to validate their email addresses with a confirmation email so
+    // this is mostly a sanity check to ensure things aren't horrible
+    // Also I wanted to use JPA validation annotations
+    @Pattern(regexp = "^.+@.+\\..+$")
+    private String email;
+
+    @NotNull
+    private String password;
+
+    // This app is intended for a replacement for on-school career fairs. Students have not graduated yet by definition,
+    // so their graduation dates are in the future implicitly.
+    @Future
+    private Date graduationDate;
+
+    @NotNull
+    private String school;
+
+    private String phoneNumber;
+
+    // TODO: Figure out what the job preferences and notification preferences will look like
+    // Pretty sure we agreed to handle them later
+
+    public long getId() {
+
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getGraduationDate() {
+        return graduationDate;
+    }
+
+    public void setGraduationDate(Date graduationDate) {
+        this.graduationDate = graduationDate;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) {
+            return true;
+        }
+        if(!(o instanceof Student)) {
+            return false;
+        }
+
+        Student student = (Student) o;
+
+        return id == student.id && email.equals(student.email);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + email.hashCode();
+        return result;
+    }
 }
