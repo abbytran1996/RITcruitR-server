f95311150591b9db15bb761aa80b22b70a74b085
Maxwell Hadley <mch6706@g.rit.edu>
2017-04-11 22:58:59 -0400

    Added storyboard layout for basic login page

---
+++ client/TMCS Client/TMCS_Client.iOS/LoginViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace TMCS_Client.iOS
+{
+    public partial class LoginViewController : UIViewController
+    {
+        public LoginViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
