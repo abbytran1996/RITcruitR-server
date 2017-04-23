381e1982c04b53ca3c5b69fab1ab9043450cace0
Zane Grasso <grassozan94@gmail.com>
2017-04-23 16:47:26 -0400

    mistakenly pushed tesxt to skillscomms - fixed

--- client/TMCS Client/TMCS_Client/ServerComms/SkillsComms.cs
+++ client/TMCS Client/TMCS_Client/ServerComms/SkillsComms.cs
@@ -8,16 +8,13 @@ namespace TMCS_Client.ServerComms
     {
         RestClient client = new RestClient(Constants.SERVER_URL);
 
-        public List<Skill> getAllSkills(){
-<<<<<<< HEAD
+		public List<Skill> getAllSkills(){
 			var request = new RestRequest(Constants.Skill.GET_SKILL_RESOURCE, Method.GET);
-=======
-            var request = new RestRequest(Constants.Skill.GET_SKILL_RESOURCE, Method.GET);
->>>>>>> d7dff2e5b11714859cf765c18040845f41d019ab
 
-            var response = client.Execute<List<Skill>>(request);
+			var response = client.Execute<List<Skill>>(request);
 
-            return response.Data;
-        }
+			return response.Data;
+			
+		}
     }
 }
