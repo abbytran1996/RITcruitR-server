03b8c09aa2bec766f974734a3c27c336ad2e9933
Maxwell Hadley <mch6706@g.rit.edu>
2017-04-25 18:30:57 -0400

    Converted client project to Xamarin.Forms project, old native project in branch

---
+++ client/TMCS_Client/TMCS_ClientPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace TMCS_Client
+{
+    public partial class TMCS_ClientPage : ContentPage
+    {
+        public TMCS_ClientPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
