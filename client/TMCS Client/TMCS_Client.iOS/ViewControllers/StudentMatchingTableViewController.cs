c8a59501a84460faa2964b7782bb75a2f24b059a
Zane Grasso <grassozan94@gmail.com>
2017-04-25 16:00:29 -0400

    created student matching tableview on strybrd as well as view&viewcontroller

---
+++ client/TMCS Client/TMCS_Client.iOS/ViewControllers/StudentMatchingTableViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace TMCS_Client.iOS
+{
+    public partial class StudentMatchingTableViewController : UITableViewController
+    {
+        public StudentMatchingTableViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
---
+++ client/TMCS Client/TMCS_Client.iOS/Views/StudentMatching.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace TMCS_Client.iOS
+{
+    public partial class StudentMatching : UITableView
+    {
+        public StudentMatching (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
