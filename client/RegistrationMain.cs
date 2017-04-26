4fc1a43a0e174ddc813345cb886de542058f5fb7
Zane Grasso <grassozan94@gmail.com>
2017-04-26 17:21:00 -0400

    Xamarin Forms, registration mainpage

---
+++ client/RegistrationMain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace client
+{
+    public class RegistrationMain : ContentPage
+    {
+        public RegistrationMain()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
---
+++ client/RegistrationMain.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace client
+{
+    public partial class RegistrationMain : ContentPage
+    {
+        public RegistrationMain()
+        {
+            
+			Label lblTitle = new Label()
+			{
+				Text = "Welcome To RecruitR!",
+				FontSize = 46.0,
+                VerticalTextAlignment = TextAlignment.Center,
+				HorizontalTextAlignment = TextAlignment.Center,
+				BackgroundColor = Color.Teal,
+
+			};
+
+
+			AbsoluteLayout roleChoiceSection = new AbsoluteLayout()
+			{
+                //BackgroundColor = Color.SandyBrown,
+			};
+
+            roleChoiceSection.Children.Add(new Label()
+            {
+                Text = "Choose A Role You Want To Register As",
+                FontSize = 25,
+                VerticalTextAlignment = TextAlignment.Center,
+                HorizontalTextAlignment = TextAlignment.Center,
+            },
+
+			new Rectangle(0.5, 0.3 , 1.0, 0.2),AbsoluteLayoutFlags.All);
+            
+
+            roleChoiceSection.Children.Add(new Button()
+            {
+                Text = "Student",
+                FontSize = 20,
+                HorizontalOptions = new LayoutOptions(LayoutAlignment.Fill, true),
+            },
+            new Rectangle(0.5, 0.6, 1.0, 0.4),AbsoluteLayoutFlags.All);
+           
+            roleChoiceSection.Children.Add(new Button()
+            {
+                Text = "Recruiter",
+                FontSize = 20,
+                HorizontalOptions = new LayoutOptions(LayoutAlignment.Fill, true),
+            },
+                                      new Rectangle(0.5, 0.9 , 1.0, 0.4), AbsoluteLayoutFlags.All);
+
+
+
+			AbsoluteLayout pageContent = new AbsoluteLayout()
+			{
+			};
+
+			pageContent.Children.Add(lblTitle, new Rectangle(0.5, 0.2, 0.7, 0.3),
+                                     AbsoluteLayoutFlags.All);
+            pageContent.Children.Add(roleChoiceSection, new Rectangle(0.5, 1.0, 1.0, 0.7),
+                                     AbsoluteLayoutFlags.All);
+
+            Content = pageContent;
+        }
+    }
+}
