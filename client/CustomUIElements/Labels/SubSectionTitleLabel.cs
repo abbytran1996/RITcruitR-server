4182cac5943abfbccd0be4d014e32a0b0576ab95
Maxwell Hadley <mch6706@g.rit.edu>
2017-04-29 16:15:36 -0400

    Job Posting Creation UI Progress

---
+++ client/CustomUIElements/Labels/SubSectionTitleLabel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TMCS_Client.CustomUIElements.Labels
+{
+    public class SubSectionTitleLabel : Label
+    {
+        public SubSectionTitleLabel(String title)
+        {
+            Text = title;
+            FontSize = 22.0;
+            VerticalTextAlignment = TextAlignment.Center;
+            HorizontalTextAlignment = TextAlignment.Center;
+        }
+    }
+}
