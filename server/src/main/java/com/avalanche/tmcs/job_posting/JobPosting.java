b3e17563aad1980b6062df40c9179d5a959b114a
David Dubois <dexcelstraun7@gmail.com>
2017-04-29 15:17:59 -0400

    Fixed a type error

--- server/src/main/java/com/avalanche/tmcs/job_posting/JobPosting.java
+++ server/src/main/java/com/avalanche/tmcs/job_posting/JobPosting.java
@@ -93,7 +93,7 @@ public class JobPosting {
     }
 
     @NotNull
-    public long getMinMatchedRequiredSkills() {
+    public int getMinMatchedRequiredSkills() {
         return minMatchedRequiredSkills;
     }
 
