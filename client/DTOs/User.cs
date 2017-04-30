a9c8c77af5c161782b621b5ba4c36dc779d735da
David Dubois <dexcelstraun7@gmail.com>
2017-04-30 12:55:26 -0400

    Added ID

--- client/DTOs/User.cs
+++ client/DTOs/User.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace TMCS_Client.DTOs {
     public class User {
-        public ind id { get; set; }
+        public int id { get; set; }
         public string username { get; set; }
         public string password { get; set; }
         public string passwordConfirm { get; set; }
