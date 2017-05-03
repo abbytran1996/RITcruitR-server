0f5776aa28b43313958b14c244042e1ce4cf891c
Maxwell Hadley <mch6706@g.rit.edu>
2017-05-03 19:54:24 -0400

    New icons

--- client/UI/RecruiterHomepage.cs
+++ client/UI/RecruiterHomepage.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Xamarin.Forms;
 using TMCS_Client.DTOs;
+using TMCS_Client.CustomUIElements.Labels;
 
 namespace TMCS_Client.UI
 {
@@ -8,10 +9,25 @@ namespace TMCS_Client.UI
     {
         private Recruiter loggedInRecruiter;
 
+        private AbsoluteLayout pageContent;
+
+        private PageTitleLabel lblTitle;
+        private ListView jobPostings;
+
         public RecruiterHomepage(Recruiter recruiter)
         {
             loggedInRecruiter = recruiter;
             this.Title = "Job Postings";
+
+            pageContent = new AbsoluteLayout();
+
+            pageContent.Children.Add(lblTitle = new PageTitleLabel("Job Postings"),
+                                    new Rectangle(0,0,1.0,Constants.Forms.Sizes.ROW_HEIGHT),
+                                     AbsoluteLayoutFlags.PositionProportional | 
+                                     AbsoluteLayoutFlags.WidthProportional);
+
+
+
 #if __IOS__
             ToolbarItem btnAddJobPosting;
             ToolbarItems.Add(btnAddJobPosting = new ToolbarItem()
