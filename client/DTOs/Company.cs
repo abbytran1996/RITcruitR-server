40a50e73b8d1434cbb59e589d1467f71f6402949
Zane Grasso <ztg6893@g.rit.edu>
2017-05-04 01:20:14 -0400

    Company fixes, database calls still not working

--- client/DTOs/Company.cs
+++ client/DTOs/Company.cs
@@ -12,12 +12,13 @@ namespace TMCS_Client.DTOs
 	public class Company
 	{
         public long id { get; set; }
+        public User user { get; set; }
         public string companyName { get; set; }
         public string emailSuffix { get; set; }
-        //public string phoneNumber { get; set; }
 		public string location { get; set; }
         public string size { get; set; }
-		//public FileStyleUriParser file { get; set; }
+        public string presentation { get; set; }
 		public string companyDescription { get; set; }
+        public Boolean approvalStatus { get; set; }
 	}
 }
