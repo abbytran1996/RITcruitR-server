7149e8784d521d76da06f94f91d85470f026aaab
David Dubois <dexcelstraun7@gmail.com>
2017-06-04 20:50:18 -0400

    Updated Match model on client code

--- client/DTOs/Match.cs
+++ client/DTOs/Match.cs
@@ -4,9 +4,29 @@ using System.Text;
 
 namespace TMCS_Client.DTOs {
     public class Match {
+        public enum ApplicationStatus {
+            NEW,
+            IN_PROGRESS,
+            ACCEPTED,
+            REJECTED
+        }
+
+        public enum CurrentPhase {
+            NONE,
+            PROBLEM,
+            PRESENTATION,
+            INTERVIEW
+        }
+
         public int id { get; set; }
         public JobPosting job { get; set; }
         public Student student { get; set; }
         public float matchStrength { get; set; }
+
+        public String studentProblemResponse { get; set; }
+        public Uri studentPresentationLink { get; set; }
+        public DateTime timeLastUpdated { get; set; }
+        public ApplicationStatus applicationStatus { get; set; }
+        public CurrentPhase currentPhase { get; set; }
     }
 }
