438cb6fb4a3cda664238b868907ea1b4d008d28a
Zane Grasso <ztg6893@g.rit.edu>
2017-06-22 14:08:50 -0400

    student presentation phase, video not working, also not connected to any other pages

---
+++ client/UI/StudentPresentationPhase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace TMCS_Client.UI
+{
+    public class StudentPresentationPhase : ContentPage
+    {
+        public StudentPresentationPhase()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
