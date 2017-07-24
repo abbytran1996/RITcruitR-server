2e19469d09214de88e93f6212c2f439c50aa69fa
John Murray <jam9608@rit.edu>
2017-07-24 16:54:50 -0400

    attempt at ios pdf viewer

--- client/UI/Recruiter/RecruiterInterviewModal.cs
+++ client/UI/Recruiter/RecruiterInterviewModal.cs
@@ -84,7 +84,36 @@ namespace TMCS_Client.UI {
                 
             }
 #elif __IOS__
+            var directory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var name = student.lastName + "resume.pdf";
+            string filePath = Path.Combine(directory.ToString(), name);
+            File.WriteAllBytes(filePath, bytes);
+            Padding = new Thickness (0, 20, 0, 0);
+            Content = new StackLayout {
+                Children = {
+                    new CustomWebView {
+                        Uri = filePath,
+                        HorizontalOptions = LayoutOptions.FillAndExpand,
+                        VerticalOptions = LayoutOptions.FillAndExpand
+                    }
+                }
+            };
 #endif
+        }
+    }
+
+
+    public class CustomWebView : WebView
+    {
+        public static readonly BindableProperty UriProperty = BindableProperty.Create(propertyName: "Uri",
+                returnType: typeof(string),
+                declaringType: typeof(CustomWebView),
+                defaultValue: default(string));
+
+        public string Uri
+        {
+            get { return (string)GetValue(UriProperty); }
+            set { SetValue(UriProperty, value); }
         }
     }
 }
