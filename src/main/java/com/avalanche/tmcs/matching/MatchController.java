31ce7b056eed32d5dbcc62e2f82fccc2bdd8e5cc
Jan Guillermo <eibweneht@gmail.com>
2018-04-23 10:42:18 -0400

    Added endpoints and removed some

--- src/main/java/com/avalanche/tmcs/matching/MatchController.java
+++ src/main/java/com/avalanche/tmcs/matching/MatchController.java
@@ -41,7 +41,7 @@ public class MatchController {
     }
 
     // ================================================================================================================
-    // * GET STUDENT MATCHES [GET]                                                                                       *
+    // * GET STUDENT MATCHES [GET]                                                                                    *
     // ================================================================================================================
     @RequestMapping(value = "/studentMatches/{id}", method = RequestMethod.GET)
     public ResponseEntity<List<Match>> getMatchesForStudent(@PathVariable long id, @RequestParam(value = "phase", defaultValue = "") String phase) {
@@ -75,28 +75,97 @@ public class MatchController {
         return ResponseEntity.ok(matches);
     }
 
-    @RequestMapping(value = "/{id}/accept", method = RequestMethod.POST)
-    public ResponseEntity<?> showInterest(@PathVariable long id, @RequestBody boolean acceptthis) {
+    // ================================================================================================================
+    // * APPROVAL FOR STUDENTS [PATCH]                                                                                *
+    // ================================================================================================================
+    @RequestMapping(value = "/{id}/approve", method = RequestMethod.PATCH)
+    public ResponseEntity<?> approveMatch(@PathVariable long id) {
         Match match = matchDAO.findOne(id);
-        if(match == null){
+
+        if (match == null){
             return ResponseEntity.notFound().build();
         }
-        if(acceptthis){
-            match.setApplicationStatus(Match.ApplicationStatus.IN_PROGRESS);
-            match.setCurrentPhase(Match.CurrentPhase.PROBLEM_WAITING_FOR_STUDENT);
-            match.setViewedSinceLastUpdate(false);
+
+        // TODO: increment stage to next
+        // increment NONE to PROBLEM
+        if (match.getCurrentPhase() == Match.CurrentPhase.NONE) {
+
+        } else {
+
         }
-        else{
-            match.setApplicationStatus(Match.ApplicationStatus.REJECTED);
-            match.setViewedSinceLastUpdate(true);
-        }
+
         match.setLastUpdatedTimeToNow();
         matchDAO.save(match);
+
         return ResponseEntity.ok().build();
     }
 
     // ================================================================================================================
-    // * GET RECRUITER MATCHES [GET]                                                                                       *
+    // * DECLINE FOR STUDENTS [PATCH]                                                                                 *
+    // ================================================================================================================
+    @RequestMapping(value = "/{id}/decline", method = RequestMethod.PATCH)
+    public ResponseEntity<?> declineMatch(@PathVariable long id) {
+        Match match = matchDAO.findOne(id);
+
+        if (match == null){
+            return ResponseEntity.notFound().build();
+        }
+
+        match.setCurrentPhase(Match.CurrentPhase.NONE);
+
+        match.setLastUpdatedTimeToNow();
+        matchDAO.save(match);
+
+        return ResponseEntity.ok().build();
+    }
+
+    // ================================================================================================================
+    // * ARCHIVE FOR STUDENTS [PATCH]                                                                                 *
+    // ================================================================================================================
+    @RequestMapping(value = "/{id}/archive", method = RequestMethod.PATCH)
+    public ResponseEntity<?> archiveMatch(@PathVariable long id) {
+        Match match = matchDAO.findOne(id);
+
+        if (match == null){
+            return ResponseEntity.notFound().build();
+        }
+
+        // checks if the match isn't in the final stage
+        if (match.getCurrentPhase() == Match.CurrentPhase.INTERVIEW) {
+            // TODO: return 300
+        }
+
+        match.setCurrentPhase(Match.CurrentPhase.ARCHIVED);
+
+        match.setLastUpdatedTimeToNow();
+        matchDAO.save(match);
+
+        return ResponseEntity.ok().build();
+    }
+
+    // ================================================================================================================
+    // * DELETE FOR STUDENTS [PATCH]                                                                                  *
+    // ================================================================================================================
+    @RequestMapping(value = "/{id}/delete", method = RequestMethod.PATCH)
+    public ResponseEntity<?> deleteMatch(@PathVariable long id) {
+        Match match = matchDAO.findOne(id);
+
+        if (match == null){
+            return ResponseEntity.notFound().build();
+        }
+
+        // checks if the match isn't archived
+        if (match.getCurrentPhase() == Match.CurrentPhase.ARCHIVED) {
+
+        }
+
+        // TODO: delete the match
+
+        return ResponseEntity.ok().build();
+    }
+
+    // ================================================================================================================
+    // * GET RECRUITER MATCHES [GET]                                                                                  *
     // ================================================================================================================
     @RequestMapping(value = "/posting/{id}", method = RequestMethod.GET)
     public ResponseEntity<List<Match>> getRecruiterMatches(@PathVariable long id, @RequestParam(value = "phase", defaultValue = "") String phase) {
@@ -125,172 +194,5 @@ public class MatchController {
         }
 
         return ResponseEntity.ok(matches);
-    }
-
-    @RequestMapping(value = "/posting/{id}/probphase", method=RequestMethod.GET)
-    public ResponseEntity<Long> getProbPhaseMatches(@PathVariable long id){
-        JobPosting job = jobDAO.findOne(id);
-        if(job ==null){
-            return ResponseEntity.notFound().build();
-        }
-        long matchesForJob = matchDAO.countAllByJobAndCurrentPhaseAndApplicationStatus(job,Match.CurrentPhase.PROBLEM_WAITING_FOR_RECRUITER,
-                Match.ApplicationStatus.IN_PROGRESS);
-
-        return ResponseEntity.ok(matchesForJob);
-    }
-
-    @RequestMapping(value = "/posting/{id}/probphase/unviewed", method=RequestMethod.GET)
-    public ResponseEntity<Long> getUnviewedProbPhaseMatches(@PathVariable long id){
-        JobPosting job = jobDAO.findOne(id);
-        if(job ==null){
-            return ResponseEntity.notFound().build();
-        }
-        long matchesForJob = matchDAO.countAllByJobAndCurrentPhaseAndApplicationStatusAndViewedSinceLastUpdateIsFalse(job,
-                Match.CurrentPhase.PROBLEM_WAITING_FOR_RECRUITER,
-                Match.ApplicationStatus.IN_PROGRESS);
-
-        return ResponseEntity.ok(matchesForJob);
-    }
-
-    @RequestMapping(value = "/posting/{id}/presentationphase", method=RequestMethod.GET)
-    public ResponseEntity<Long> getPresentationPhaseMatches(@PathVariable long id){
-        JobPosting job = jobDAO.findOne(id);
-        if(job ==null){
-            return ResponseEntity.notFound().build();
-        }
-        long matchesForJob = matchDAO.countAllByJobAndCurrentPhaseAndApplicationStatus(job,Match.CurrentPhase.PRESENTATION_WAITING_FOR_RECRUITER,
-                Match.ApplicationStatus.IN_PROGRESS);
-
-        return ResponseEntity.ok(matchesForJob);
-    }
-
-    @RequestMapping(value = "/{jobPostingID}/problemResponsePending", method = RequestMethod.GET)
-    public ResponseEntity<List<Match>> getMatchesWithProblemResponsePending(@PathVariable long jobPostingID){
-        JobPosting job = jobDAO.findOne(jobPostingID);
-        List<Match> matches = matchDAO.findAllByJobAndCurrentPhaseAndApplicationStatus(job,
-                Match.CurrentPhase.PROBLEM_WAITING_FOR_RECRUITER,
-                Match.ApplicationStatus.IN_PROGRESS);
-        for(Match m : matches){
-            m.setViewedSinceLastUpdate(true);
-        }
-        matchDAO.save(matches);
-
-        return ResponseEntity.ok(matches);
-    }
-
-    @RequestMapping(value = "/{jobPostingId}/presentationResponsePending", method = RequestMethod.GET)
-    public ResponseEntity<List<Match>> getMatchesWithPresentationResponsePending(@PathVariable long jobPostingId) {
-        JobPosting job = jobDAO.findOne(jobPostingId);
-        if(job == null) {
-            return ResponseEntity.notFound().build();
-        }
-
-        List<Match> matches = matchDAO.findAllByJobAndCurrentPhaseAndApplicationStatus(job,
-                Match.CurrentPhase.PRESENTATION_WAITING_FOR_RECRUITER,
-                Match.ApplicationStatus.IN_PROGRESS);
-        for(Match m : matches){
-            m.setViewedSinceLastUpdate(true);
-        }
-        matchDAO.save(matches);
-
-        return ResponseEntity.ok(matches);
-    }
-
-    @RequestMapping(value="/{jobPostingID}s/interviewPhaseMatches/Count", method= RequestMethod.GET)
-    public ResponseEntity<Long> getInterviewPhaseMatchesCount(@PathVariable long jobPostingID){
-        JobPosting job = jobDAO.findOne(jobPostingID);
-        long matches = matchDAO.countAllByJobAndCurrentPhaseAndApplicationStatus(job,
-                Match.CurrentPhase.INTERVIEW,
-                Match.ApplicationStatus.ACCEPTED);
-
-        return ResponseEntity.ok(matches);
-    }
-
-    @RequestMapping(value="/{jobPostingID}/interviewPhaseMatches/unviewed/Count", method= RequestMethod.GET)
-    public ResponseEntity<Long> getUnviewedInterviewPhaseMatchesCount(@PathVariable long jobPostingID){
-        JobPosting job = jobDAO.findOne(jobPostingID);
-        long matches = matchDAO.countAllByJobAndCurrentPhaseAndApplicationStatusAndViewedSinceLastUpdateIsFalse(job,
-                Match.CurrentPhase.INTERVIEW,
-                Match.ApplicationStatus.ACCEPTED);
-
-        return ResponseEntity.ok(matches);
-    }
-
-    @RequestMapping(value="/{jobPostingID}/presentationPhaseMatches/Count", method= RequestMethod.GET)
-    public ResponseEntity<Long> getPresentationPhaseMatchesCount(@PathVariable long jobPostingID){
-        JobPosting job = jobDAO.findOne(jobPostingID);
-        long matches = matchDAO.countAllByJobAndCurrentPhaseAndApplicationStatus(job,
-                Match.CurrentPhase.PRESENTATION_WAITING_FOR_RECRUITER,
-                Match.ApplicationStatus.IN_PROGRESS);
-
-        return ResponseEntity.ok(matches);
-    }
-
-    @RequestMapping(value="/{jobPostingID}/presentationPhaseMatches/unviewed/Count", method= RequestMethod.GET)
-    public ResponseEntity<Long> getUnviewedPresentationPhaseMatchesCount(@PathVariable long jobPostingID){
-        JobPosting job = jobDAO.findOne(jobPostingID);
-        long matches = matchDAO.countAllByJobAndCurrentPhaseAndApplicationStatusAndViewedSinceLastUpdateIsFalse(job,
-                Match.CurrentPhase.PRESENTATION_WAITING_FOR_RECRUITER,
-                Match.ApplicationStatus.IN_PROGRESS);
-
-        return ResponseEntity.ok(matches);
-    }
-
-    @RequestMapping(value="/{jobPostingID}/interviewPhaseMatches", method= RequestMethod.GET)
-    public ResponseEntity<List<Match>> getInterviewPhaseMatches(@PathVariable long jobPostingID){
-        JobPosting job = jobDAO.findOne(jobPostingID);
-        List<Match> matches = matchDAO.findAllByJobAndCurrentPhaseAndApplicationStatus(job,
-                Match.CurrentPhase.INTERVIEW,
-                Match.ApplicationStatus.ACCEPTED);
-        for(Match m : matches){
-            m.setViewedSinceLastUpdate(true);
-        }
-        matchDAO.save(matches);
-
-        return ResponseEntity.ok(matches);
-    }
-
-    @RequestMapping(value = "/{id}/update", method = RequestMethod.PATCH)
-    public ResponseEntity<Boolean> updateMatch(@PathVariable long id, @RequestBody Match match){
-        if(id == match.getId()){
-            match.setViewedSinceLastUpdate(false);
-            match.setLastUpdatedTimeToNow();
-            matchDAO.save(match);
-        }
-
-        return ResponseEntity.ok(true); //TODO make the result variable
-    }
-
-    @RequestMapping(value = "/{id}/submitresponse/{response}", method = RequestMethod.POST)
-    public ResponseEntity<?> setProblemResponse(@PathVariable long id, @PathVariable String response) {
-        Match match = matchDAO.findOne(id);
-        if(match == null) {
-            return ResponseEntity.notFound().build();
-        }
-
-        match.setStudentProblemResponse(response);
-        match.setCurrentPhase(Match.CurrentPhase.PROBLEM_WAITING_FOR_RECRUITER);
-        match.setViewedSinceLastUpdate(false);
-        match.setLastUpdatedTimeToNow();
-        matchDAO.save(match);
-
-        return ResponseEntity.ok().build();
-    }
-
-
-    @RequestMapping(value = "/{id}/submitlink/{link}", method = RequestMethod.POST)
-    public ResponseEntity<?> setStudentLink(@PathVariable long id, @PathVariable String link) {
-        Match match = matchDAO.findOne(id);
-        if(match == null) {
-            return ResponseEntity.notFound().build();
-        }
-
-        match.setStudentPresentationLink(link);
-        match.setCurrentPhase(Match.CurrentPhase.PRESENTATION_WAITING_FOR_RECRUITER);
-        match.setViewedSinceLastUpdate(false);
-        match.setLastUpdatedTimeToNow();
-        matchDAO.save(match);
-
-        return ResponseEntity.ok().build();
     }
 }
