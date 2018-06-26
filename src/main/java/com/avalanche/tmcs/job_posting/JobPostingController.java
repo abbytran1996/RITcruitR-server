5b029d690bf81c374b4b8c7cb446d141fb3afeca
Aidan <aks5238@rit.edu>
2018-06-26 07:18:30 -0400

    REC-169: [bugfix] forgot to include an import

--- src/main/java/com/avalanche/tmcs/job_posting/JobPostingController.java
+++ src/main/java/com/avalanche/tmcs/job_posting/JobPostingController.java
@@ -7,6 +7,7 @@ import com.avalanche.tmcs.recruiter.Recruiter;
 import com.avalanche.tmcs.matching.MatchingService;
 import com.avalanche.tmcs.recruiter.RecruiterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
