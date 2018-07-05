23d40c7b0c3aece49163d4145b73c54d7e1dc1f1
Ryan Hochmuth <rhochmuth@Ryans-MacBook-Pro-2.local>
2018-07-04 21:04:20 -0400

    REC-180: Updated job update endpoint
    - Presentation links are now properly updated
      with the job update endpoint.

---
+++ src/main/java/com/avalanche/tmcs/job_posting/JobPresentationLinkDAO.java
@@ -0,0 +1,9 @@
+package com.avalanche.tmcs.job_posting;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface JobPresentationLinkDAO extends CrudRepository<JobPresentationLink, Long> {
+
+}
