468e10fcf5dac01c71fc56ad1f56739cc8f46123
Aidan <aks5238@rit.edu>
2018-07-10 16:20:21 -0400

    REC-64: complete switch to parallelStream (old tests)
    - split shared functionality into static helper
    - hook up old caller and replace outdated helpers

--- src/main/java/com/avalanche/tmcs/matching/MatchingService.java
+++ src/main/java/com/avalanche/tmcs/matching/MatchingService.java
@@ -30,11 +30,7 @@ public class MatchingService {
     private JobPostingDAO jobPostingDAO;
 
     private Executor executor = Executors.newFixedThreadPool(10); //TODO replace with parallelStream
-
     private final static float REQUIRED_SKILL_WEIGHT = 0.8f;
-    private final static float DEFAULT_OPTIONAL_SKILL_WEIGHT = 0.4f;
-    private final static float RECRUITER_PREFERENCE_WEIGHT = 0.5f;
-    private final static float STUDENT_PREFERENCE_WEIGHT = 0.4f;
 
     @Autowired
     public MatchingService(MatchDAO matchDAO, StudentDAO studentDAO, JobPostingDAO jobPostingDAO) {
@@ -43,157 +39,98 @@ public class MatchingService {
         this.jobPostingDAO = jobPostingDAO;
     }
 
-    public List<Match> generateMatchesForStudent(final Student student) {
-        // create new matches for a student from all currently active jobs
-        final Set<JobPosting> jobsStudentAlreadyMatchedWith = matchDAO.findAllByStudent(student).parallelStream()
-                .map(Match::getJob)
-                .collect(Collectors.toSet());
-        List<JobPosting> currentlyActiveJobs = jobPostingDAO.findAllByStatus(JobPosting.Status.ACTIVE.toInt());
-        Set<Skill> studentSkills = student.getSkills();
-
-        List<Match> newMatches = new ArrayList<Match>();
-        double matchScore = 0;
-        for(JobPosting job : currentlyActiveJobs){
-            // do not recalculate the matchScore for jobs the student is already matched to
-            if(jobsStudentAlreadyMatchedWith.contains(job))
-                continue;
-
-            double requiredSkillsScore = weightedPercentageSetIntersection(
-                    job.getRequiredSkills(),
-                    studentSkills,
-                    REQUIRED_SKILL_WEIGHT
-            );
-
-            double recommendedSkillsWeight = job.getRecommendedSkillsWeight();
-            double recommendedSkillsScore = weightedPercentageSetIntersection(
-                    job.getRecommendedSkills(),
-                    studentSkills,
-                    (float) recommendedSkillsWeight
-            );
-
-            double jobFilterWeight = job.getJobFiltersWeight();
-            double jobFilterScore = job.calculateJobFiltersScore(student);
-
-            double studentPreferencesWeight = student.getStudentPreferencesWeight();
-            double studentPreferencesScore = student.calculateStudentPreferencesScore(job);
-
-
-            double normalizedWeightDenominator = REQUIRED_SKILL_WEIGHT+recommendedSkillsWeight+jobFilterWeight+studentPreferencesWeight;
-            matchScore = (requiredSkillsScore+recommendedSkillsScore+jobFilterScore+studentPreferencesScore) / normalizedWeightDenominator;
-
-            if(matchScore >= job.getMatchThreshold()){
-                Match newMatch = new Match()
-                    .setJob(job)
-                    .setStudent(student)
-                    .setMatchStrength((float) matchScore);
-                newMatches.add(newMatch);
-            }
-        }
-        return newMatches;
-    }
-
     /**
      * Registers the given student with the matching student
-     * <p>This adds the student to the skills index and calculates the best matches for this student</p>
-     * <p>The expectation is that this method will be called when a new student is registered</p>
-     * <p>This method uses a thread to perform the actual matching, so calling it should be very fast</p>
-     *
+     * <p>This calculates the best matches for this student</p>
      * @param student The student to register
      */
     public void registerStudent(final Student student) {
-        executor.execute(() -> {
-            final Map<JobPosting, MatchedSkillsCount> matchedSkillsCountMap = new HashMap<>();
-            final List<Match> studentMatches = matchDAO.findAllByStudent(student);
-            Predicate<JobPosting> newJobFilter = posting -> studentMatches.stream().noneMatch(match -> match.getJob().equals(posting));
-            for (Skill skill : student.getSkills()) {
-                // This does one DB call for each skill in the student's profile
-                // Based on what I've seen on LinkedIn, I'd expect students to have maybe a hundred skill max? That's
-                // 100 calls. Would be better if we didn't need that many, but should be fine for a POC
-                List<JobPosting> postingsWithAtLeastOneRequiredSkill = jobPostingDAO.findAllByRequiredSkillsContains(skill);
-                postingsWithAtLeastOneRequiredSkill.stream()
-                        .filter(newJobFilter)
-                        .forEach(posting -> {
-                            if(!matchedSkillsCountMap.containsKey(posting)) {
-                                matchedSkillsCountMap.put(posting, new MatchedSkillsCount());
-                            }
-                            matchedSkillsCountMap.get(posting).requiredSkillsCount += 1;
-                        });
-
-                List<JobPosting> postingsWithAtLeastOneRecommendedSkill = jobPostingDAO.findAllByNiceToHaveSkillsContains(skill);
-                postingsWithAtLeastOneRecommendedSkill.stream()
-                        .filter(newJobFilter)
-                        .forEach(posting -> {
-                            if(!matchedSkillsCountMap.containsKey(posting)) {
-                                matchedSkillsCountMap.put(posting, new MatchedSkillsCount());
-                            }
-                            matchedSkillsCountMap.get(posting).recommendedSkillsCount += 1;
-                        });
-            }
-
-            final List<Match> matches = buildMatchesList(student, matchedSkillsCountMap);
-
-            matchDAO.save(matches);
-        });
+        final List<Match> matches = generateMatchesForStudent(student);
+        matchDAO.save(matches);
     }
 
     /**
      * Registers the given job posting with the matching service
-     * <p>This adds the job posting to the skills index and calculates the best matches for this job posting</p>
-     * <p>The expectation is that this method will be called when a job posting is registered</p>
-     * <p>This method uses a thread to perform the actual matching, so calling it should be very fast</p>
-     *
+     * <p>This calculates the best matches for this job posting</p>
      * @param posting The job posting to register
      */
     public void registerJobPosting(final JobPosting posting) {
-        executor.execute(() -> {
-            final Map<Student, Integer> numberOfMatchedRequiredSkills = countStudentsWithSkillInList(posting.getRequiredSkills());
-            final Map<Student, Integer> numberOfMatchedRecommendedSkills = countStudentsWithSkillInList(posting.getRecommendedSkills());
-
-            final List<Match> matches = new ArrayList<>();
-            for(Student student : numberOfMatchedRequiredSkills.keySet()) {
-                double weight = numberOfMatchedRequiredSkills.get(student) * REQUIRED_SKILL_WEIGHT;
-
-                if(numberOfMatchedRecommendedSkills.containsKey(student)) {
-                    weight += numberOfMatchedRecommendedSkills.get(student) * (1.0f - REQUIRED_SKILL_WEIGHT);
-                }
-
-                if(weight >= REQUIRED_SKILL_WEIGHT) {
-                    Match match = new Match();
-                    match.setStudent(student);
-                    match.setJob(posting);
-                    match.setMatchStrength((float) weight);
-
-                    matches.add(match);
-                }
-            }
-
-            matchDAO.save(matches);
-        });
+        final List<Match> matches = generateMatchesForJob(posting);
+        matchDAO.save(matches);
     }
 
-    /**
-     * Gets all the students with at least one skill on the list of skills and counts how many skills that student has
-     *
-     * @param skills The list of skills to look for students with
-     * @return A map from students who have at least one skill on the list to the number of skills on the list they have
-     */
-    Map<Student, Integer> countStudentsWithSkillInList(final Iterable<Skill> skills) {
-        final Map<Student, Integer> skillsCount = new HashMap<>();
-        for(Skill skill : skills) {
-            Set<Student> studentsWithSkill = studentDAO.findAllBySkillsContains(skill);
+    public List<Match> generateMatchesForStudent(final Student student) {
+        // create new matches for a student from all currently active jobs
+        List<Match> matchesToReturn = jobPostingDAO.findAllByStatus(JobPosting.Status.ACTIVE.toInt()).parallelStream()
+                .map(job -> generateMatchForStudentAndJob(student, job))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toList());
 
-            for(Student s : studentsWithSkill) {
-                Integer val = 0;
-                if(skillsCount.containsKey(s)) {
-                    val = skillsCount.get(s);
-                }
+        // re-add matches that are already in or past the FINAL stage
+        final Set<Match> preexistingFinalMatches = matchDAO.findAllByStudent(student).parallelStream()
+                .filter(match -> match.getCurrentPhase() == Match.CurrentPhase.FINAL ||
+                        match.getCurrentPhase() == Match.CurrentPhase.ARCHIVED)
+                .collect(Collectors.toSet());
+        matchesToReturn.addAll(preexistingFinalMatches);
+        return matchesToReturn;
+    }
 
-                skillsCount.put(s, val + 1);
-            }
+    public List<Match> generateMatchesForJob(final JobPosting job){
+        // create new matches for a job from all current students
+        List<Match> matchesToReturn = studentDAO.findAll().parallelStream()
+                .map(student -> generateMatchForStudentAndJob(student, job))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toList());
+
+        // re-add matches that are already in or past the FINAL stage
+        Set<Match> preexistingFinalMatches = matchDAO.findAllByJob(job).parallelStream()
+                .filter(match -> match.getCurrentPhase() == Match.CurrentPhase.FINAL ||
+                        match.getCurrentPhase() == Match.CurrentPhase.ARCHIVED)
+                .collect(Collectors.toSet());
+        matchesToReturn.addAll(preexistingFinalMatches);
+        return matchesToReturn;
+    }
+
+
+    private static Optional<Match> generateMatchForStudentAndJob(final Student student, final JobPosting job){
+        Set<Skill> studentSkills = student.getSkills();
+        double matchScore = 0;
+        double requiredSkillsScore = weightedPercentageSetIntersection(
+                job.getRequiredSkills(),
+                studentSkills,
+                REQUIRED_SKILL_WEIGHT
+        );
+
+        double recommendedSkillsWeight = job.getRecommendedSkillsWeight();
+        double recommendedSkillsScore = weightedPercentageSetIntersection(
+                job.getRecommendedSkills(),
+                studentSkills,
+                (float) recommendedSkillsWeight
+        );
+
+        double jobFilterWeight = job.getJobFiltersWeight();
+        double jobFilterScore = job.calculateJobFiltersScore(student);
+
+        double studentPreferencesWeight = student.getStudentPreferencesWeight();
+        double studentPreferencesScore = student.calculateStudentPreferencesScore(job);
+
+
+        double normalizedWeightDenominator = REQUIRED_SKILL_WEIGHT+recommendedSkillsWeight+jobFilterWeight+studentPreferencesWeight;
+        matchScore = (requiredSkillsScore+recommendedSkillsScore+jobFilterScore+studentPreferencesScore) / normalizedWeightDenominator;
+
+        if(matchScore >= job.getMatchThreshold()){
+            Match newMatch = new Match()
+                    .setJob(job)
+                    .setStudent(student)
+                    .setMatchStrength((float) matchScore);
+            return Optional.of(newMatch);
+        } else {
+            return Optional.empty();
         }
-
-        return skillsCount;
     }
+
 
     /**
      * Builds the list of matches between students and potential jobs for that student
@@ -204,12 +141,12 @@ public class MatchingService {
      */
     List<Match> buildMatchesList(final Student student, final Map<JobPosting, MatchedSkillsCount> matchedSkillsCountMap) {
         final List<Match> matches = new ArrayList<>();
-        for(JobPosting posting : matchedSkillsCountMap.keySet()) {
+        for (JobPosting posting : matchedSkillsCountMap.keySet()) {
             MatchedSkillsCount matchedSkillsCount = matchedSkillsCountMap.get(posting);
             double numRequiredSkills = posting.getRequiredSkills().size();
             double weight = matchedSkillsCount.requiredSkillsCount * REQUIRED_SKILL_WEIGHT / numRequiredSkills;
             int numRecommendedSkills = posting.getRecommendedSkills().size();
-            if(numRecommendedSkills > 0) {
+            if (numRecommendedSkills > 0) {
                 weight += matchedSkillsCount.recommendedSkillsCount * (1.0f - REQUIRED_SKILL_WEIGHT) / numRecommendedSkills;
             }
 
@@ -224,9 +161,33 @@ public class MatchingService {
         return matches;
     }
 
+    /**
+     * Gets all the students with at least one skill on the list of skills and counts how many skills that student has
+     *
+     * @param skills The list of skills to look for students with
+     * @return A map from students who have at least one skill on the list to the number of skills on the list they have
+     */
+    Map<Student, Integer> countStudentsWithSkillInList(final Iterable<Skill> skills) {
+        final Map<Student, Integer> skillsCount = new HashMap<>();
+        for (Skill skill : skills) {
+            Set<Student> studentsWithSkill = studentDAO.findAllBySkillsContains(skill);
+
+            for (Student s : studentsWithSkill) {
+                Integer val = 0;
+                if (skillsCount.containsKey(s)) {
+                    val = skillsCount.get(s);
+                }
+
+                skillsCount.put(s, val + 1);
+            }
+        }
+        return skillsCount;
+    }
+
     static class MatchedSkillsCount {
         int requiredSkillsCount = 0;
         int recommendedSkillsCount = 0;
     }
+
 }
 
